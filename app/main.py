4ad9217159306ef52a4589984850adf47fd93b4b
avozniuk <a.o.vozniuk@gmail.com>
2025-10-01 19:10:23 +0300

    update

--- app/main.py
+++ app/main.py
@@ -4,20 +4,9 @@ class Animal:
     def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
         self.hidden = False
-        self._health = 100
         self.health = health  # go through setter
         if self.health > 0:
             Animal.alive.append(self)
-
-    @property
-    def health(self) -> int:
-        return self._health
-
-    @health.setter
-    def health(self, value: int) -> None:
-        self._health = max(0, value)
-        if self._health == 0 and self in Animal.alive:
-            Animal.alive.remove(self)
 
     def __repr__(self) -> str:
         return (f"{{Name: {self.name}, Health: {self.health}, "
